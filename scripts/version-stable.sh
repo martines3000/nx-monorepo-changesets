791d3cfcf41137d01677194e48e63d81d9ba783b
martines3000 <domajnko.martin@gmail.com>
2023-07-28 12:35:08 +0200

    ci: split scripts into stable and beta

---
+++ scripts/version-stable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Updating beta version..."
+
+# First we exit rc mode and then we version the packages
+
+pnpm changeset pre exit && \
+  pnpm changeset version && \
+  pnpm install --lockfile-only && \
+  pnpm build && \
+  git add . && \
+  git commit -m \"chore: update versions\" && \
+  echo "Successfully updated stable version!" || \
+  echo "Failed to update stable version!"
+
